f1fd14b4e1f3f3674fa5eb5ead97ac1a21c8bf63
Miqueas Garcia <mqggonzalez@gmail.com>
2024-12-10 11:56:47 +0000

    Added doc

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,85 @@
+
 const catBreedInfoURL = "https://api.thecatapi.com/v1/breeds"; 
 let breedsJSON = [];
 let breeds = [];
 let selectedBreedName;
 let selectedBreedID;
 
+/**
+ * The function `storeJSON` asynchronously fetches JSON data from a specified URL.
+ * @returns The `storeJSON` function is returning a Promise that resolves to the
+ * JSON data fetched from the `catBreedInfoURL`.
+ */
 async function storeJSON() {
   const request = new Request(catBreedInfoURL);
   const response = await fetch(request);
 
-  return await response.json(); // Devuelve el JSON de la API
+  return await response.json();
 }
 
+/**
+ * The `onLoad` function asynchronously loads JSON data, maps it to extract
+ * specific information(breedid, breedname and imgid),
+ * populates a dropdown menu, adds an event listener for
+ * dropdown changes, and logs the extracted data.
+ */
 async function onLoad() {
-  // Espera a que los datos se descarguen antes de usarlos
   breedsJSON = await storeJSON(); 
 
-  // Obtiene los nombres, id y imgid
   breeds = breedsJSON.map(cat => [cat.id, cat.name, cat.reference_image_id]);
 
   populateDropdown();
 
-  // Escuchar cambios en el dropdown
   const dropdown = document.getElementById('cat-breeds');
   dropdown.addEventListener('change', handleSelection);
 
   console.log(breeds);
 }
 
-// Función para popular el menú desplegable
+/**
+ * The function `populateDropdown` populates dropdown menu with options based on
+ * the `breeds` array.
+ */
 function populateDropdown() {
   const dropdown = document.getElementById('cat-breeds');
   
-  // Añade opciones al menú desplegable
   breeds.forEach(([id, name]) => {
     const option = document.createElement('option');
-    option.value = id; // Usamos el 'id' como valor
-    option.textContent = name; // Mostramos el nombre de la raza
+    option.value = id;
+    option.textContent = name; 
     dropdown.appendChild(option);
   });
 }
 
-// Función para manejar la selección del dropdown
+
+/**
+ * The function `handleSelection` captures the selected breed ID and name from a
+ * dropdown menu and logs the selected breed information.
+ * @param event - The `event` parameter in the `handleSelection` function is an
+ * object that represents the event that occurred, such as a user selecting an
+ * option from a dropdown menu. It contains information about the event, including
+ * the target element that triggered the event (in this case, a dropdown menu), the
+ * type
+ */
 function handleSelection(event) {
-  selectedBreedID = event.target.value; // Obtiene el 'value' de la opción seleccionada
-  selectedBreedName = event.target.options[event.target.selectedIndex].text; // Obtiene el texto visible
+  selectedBreedID = event.target.value; 
+  selectedBreedName = event.target.options[event.target.selectedIndex].text; 
 
   console.log(`Selected Breed: ${selectedBreedName} (ID: ${selectedBreedID})`);
   findBreed(selectedBreedID);
 }
 
-// Llama a onLoad cuando cargue la página
 onLoad();
 
-//Find cat's breed info
+
+/**
+ * The function `findBreed` makes a fetch request to the Cat API to retrieve
+ * information about a specific cat breed using the provided `breedID`.
+ * @param breedID - The `breedID` parameter in the `findBreed` function is used to
+ * specify the ID of the cat breed that you want to retrieve information about from
+ * the Cat API. This ID is used to construct the URL for the API request to fetch
+ * data about the specific cat breed.
+ */
 function findBreed(breedID){
   let urlAPI = `https://api.thecatapi.com/v1/breeds/${breedID}`;
 
@@ -62,7 +89,15 @@ function findBreed(breedID){
   .catch(error => showError(error));
 }
 
-// Show breed info
+
+/**
+ * The function `showBreed` displays information about a specific dog breed,
+ * including its name, image, and ratings for various characteristics.
+ * @param breedData - The `showBreed` function takes in a `breedData` object as a
+ * parameter. This object likely contains information about a specific dog breed,
+ * such as its name, image URL, and various ratings related to adaptability,
+ * child-friendliness, dog-friendliness, energy level,
+ */
 function showBreed(breedData){
   let infoDivID = `breedInfo`;
   let infoDiv = document.getElementById(infoDivID);
@@ -78,15 +113,32 @@ function showBreed(breedData){
   `;
 }
 
+/**
+ * The function getImageURL takes an imageID as input and returns a URL for a cat
+ * image with that ID.
+ * @param imageID - The `imageID` parameter is a unique identifier for a specific
+ * image in the Cat API.
+ * @returns a URL for a cat image based on the provided `imageID`.
+ */
 function getImageURL(imageID){
   console.log(imageID);
   return `https://cdn2.thecatapi.com/images/${imageID}.jpg`;
 }
 
+/**
+ * The function showError logs an error message to the console.
+ * @param error - The `error` parameter in the `showError` function is used to pass
+ * an error message or object that needs to be displayed or logged.
+ */
 function showError(error){
   console.log(error);
 }
 
+/**
+ * The function getRating returns a set of (filled/empty) stars.
+ * @param {*} rating - Expected number out of 5
+ * @returns a String of (filled/empty) stars.
+ */
 function getRating(rating) {
   let result = "";
 
